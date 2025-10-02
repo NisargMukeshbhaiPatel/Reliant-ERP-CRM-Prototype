d3eff0dee0b57c39f91d4e901b9b6dda7142fbb7
Nisarg Mukeshbhai Patel <101616954+NisargMukeshbhaiPatel@users.noreply.github.com>
2025-10-03 02:05:49 +1000

    Add back to products btn in auth pages

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import { Button } from "@/components/button";
+import { ArrowLeft } from "lucide-react";
+
 export default async function RootLayout({ children }) {
-  return <main>{children}</main>;
+  return (
+    <main className="relative">
+      <div className="absolute left-4 top-4 z-10">
+        <Link href="/">
+          <Button variant="default" size="lg">
+            <ArrowLeft className="h-5 w-5" />
+            Go Back to Products
+          </Button>
+        </Link>
+      </div>
+      {children}
+    </main>
+  );
 }
